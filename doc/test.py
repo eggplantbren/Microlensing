f51479deda1e793e761f02a321e2481b174f801e
Brendon Brewer <brendon.brewer@gmail.com>
2015-09-23 14:16:14 +0100

    Got them to agree

--- doc/test.py
+++ doc/test.py
@@ -1,8 +1,8 @@
 # Quick Python test to make sure I got my derivation correct
 from pylab import *
 
-seed(0)
 # Some data
+seed(0)
 N = 101
 t = linspace(0, 10, N)
 sig = -log(rand(N))
@@ -12,11 +12,10 @@ errorbar(t, y, yerr=sig, fmt='b.')
 show()
 
 # Stuff derived from the data
-C = exp(-0.5*N*log(2*pi) - sum(log(sig)))
+C = prod(1./sig)*(2*pi)**(-0.5*N)
 alpha = sum((y - mprime)**2/sig**2)
 beta = -2.*sum((y - mprime)/sig**2)
 gamma = sum(1./sig**2)
-
 
 # For the numerical marginalisation
 b = linspace(-20, 20, 10001)
@@ -26,17 +25,16 @@ L = b.max() - b.min()
 # otherwise, it's marginalised over b
 def log_likelihood(b=None):
 	if b is not None:
-		chisq = sum((y - (mprime + b))**2)
+		chisq = sum((y - (mprime + b))**2/sig**2)
 		return log(C) - 0.5*chisq
 
 # The numerical marginalisation
 logL = zeros(len(b))
 for i in range(0, len(b)):
 	logL[i] = log_likelihood(b[i])
-print(trapz(exp(logL), x=b))
+print(trapz(exp(logL)/L, x=b))
 
 # The analytical marginalisation
 result = C/L*exp(-0.5*gamma*(alpha/gamma - beta**2/4/gamma**2))*sqrt(2*pi/gamma)
 print(result)
-
 
